9c6e7af9966041398696b01343981ff0bbb4ea8b
Kevin pedroza <kevin.andres01@hotmail.com>
2018-12-14 09:17:40 -0500

    sc1

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, ssl_context=('cert.pem', 'key.pem'), port=5001)
+    app.run(host='0.0.0.0', debug=True, ssl_context=('/home/ubuntu/.ssh/cert.pem', '/home/ubuntu/.ssh/key.pem'), port=5001)
